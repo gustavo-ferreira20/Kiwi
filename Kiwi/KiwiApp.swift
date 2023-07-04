e89dd35620e731412bc4d891241535c1e36429cd
Gustavo Rodrigues <ferreirasouza20@hotmail.com>
2023-07-04 10:10:32 +0800

    Initial Commit

---
+++ Kiwi/KiwiApp.swift
@@ -0,0 +1,17 @@
+//
+//  KiwiApp.swift
+//  Kiwi
+//
+//  Created by Gustavo rodrigues on 2023/7/4.
+//
+
+import SwiftUI
+
+@main
+struct KiwiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
