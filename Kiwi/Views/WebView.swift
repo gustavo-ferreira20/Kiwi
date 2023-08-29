b529977b43070f19e763c7e5459bd602eee9b070
Gustavo Rodrigues <ferreirasouza20@hotmail.com>
2023-08-29 15:58:05 +0800

    Video list background fixed

---
+++ Kiwi/Views/WebView.swift
@@ -0,0 +1,26 @@
+//
+//  WebView.swift
+//  Kiwi
+//
+//  Created by Gustavo rodrigues on 2023/8/28.
+//
+
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    let urlString: String
+
+    func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        return webView
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let url = URL(string: urlString) {
+            let request = URLRequest(url: url)
+            uiView.load(request)
+        }
+    }
+}
+
